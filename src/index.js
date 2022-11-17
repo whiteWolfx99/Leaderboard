711d07c0a8ef167178bd75ebb1eb1e4a15068d28
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-17 11:28:21 +0300

    hit api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import './style.css';
-import './Leaderboard.js';
+import Leaderboard from './Leaderboard.js';
+import MakeApicall from './url.js';
+
+const scorelist = document.querySelector('.scorelist');
+const refresherbutton = document.querySelector('.refresh');
+const addbutton = document.querySelector('.add');
+const api = new MakeApicall();
+
+const refresher = async () => {
+  scorelist.innerHTML = '';
+  const res = await api.getmethod();
+  const Leaderboards = new Leaderboard(res);
+
+  Leaderboards.username.forEach((usernames) => {
+    scorelist.innerHTML += `
+    ${
+  usernames.user
+    ? `<li class="li">${usernames.user} : ${usernames.score}</li>`
+    : 'No data yet'
+} `;
+  });
+};
+
+refresherbutton.addEventListener('click', () => {
+  refresher();
+});
+
+addbutton.addEventListener('click', async () => {
+  const username = document.querySelector('.name').value;
+  const mark = document.querySelector('.score').value;
+  await api.postmethod(username, mark);
+  document.querySelector('.name').value = '';
+  document.querySelector('.score').value = '';
+  refresher();
+});
+
+document.addEventListener('DOMContentLoaded', refresher, false);
